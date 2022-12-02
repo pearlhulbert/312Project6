14cf21a0fe63071ac44c1830a90f9f434ac53a67
eagledb14 <eagledb14@gmail.com>
2022-12-02 16:12:14 -0700

    greedy now returns all found valid paths

--- TSPSolver.py
+++ TSPSolver.py
@@ -2,11 +2,11 @@
 
 from which_pyqt import PYQT_VER
 if PYQT_VER == 'PYQT5':
-	from PyQt5.QtCore import QLineF, QPointF
+    from PyQt5.QtCore import QLineF, QPointF
 elif PYQT_VER == 'PYQT4':
-	from PyQt4.QtCore import QLineF, QPointF
+    from PyQt4.QtCore import QLineF, QPointF
 else:
-	raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
+    raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
 
 
 
@@ -20,66 +20,66 @@ import itertools
 
 
 class TSPSolver:
-	def __init__( self, gui_view ):
-		self._scenario = None
+    def __init__( self, gui_view ):
+        self._scenario = None
 
-	def setupWithScenario( self, scenario ):
-		self._scenario = scenario
-
-
-	''' <summary>
-		This is the entry point for the default solver
-		which just finds a valid random tour.  Note this could be used to find your
-		initial BSSF.
-		</summary>
-		<returns>results dictionary for GUI that contains three ints: cost of solution, 
-		time spent to find solution, number of permutations tried during search, the 
-		solution found, and three null values for fields not used for this 
-		algorithm</returns> 
-	'''
-	
-	def defaultRandomTour( self, time_allowance=60.0 ):
-		results = {}
-		cities = self._scenario.getCities()
-		ncities = len(cities)
-		foundTour = False
-		count = 0
-		bssf = None
-		start_time = time.time()
-		while not foundTour and time.time()-start_time < time_allowance:
-			# create a random permutation
-			perm = np.random.permutation( ncities )
-			route = []
-			# Now build the route using the random permutation
-			for i in range( ncities ):
-				route.append( cities[ perm[i] ] )
-			bssf = TSPSolution(route)
-			count += 1
-			if bssf.cost < np.inf:
-				# Found a valid route
-				foundTour = True
-		end_time = time.time()
-		results['cost'] = bssf.cost if foundTour else math.inf
-		results['time'] = end_time - start_time
-		results['count'] = count
-		results['soln'] = bssf
-		results['max'] = None
-		results['total'] = None
-		results['pruned'] = None
-		return results
+    def setupWithScenario( self, scenario ):
+        self._scenario = scenario
 
 
-	''' <summary>
-		This is the entry point for the greedy solver, which you must implement for 
-		the group project (but it is probably a good idea to just do it for the branch-and
-		bound project as a way to get your feet wet).  Note this could be used to find your
-		initial BSSF.
-		</summary>
-		<returns>results dictionary for GUI that contains three ints: cost of best solution, 
-		time spent to find best solution, total number of solutions found, the best
-		solution found, and three null values for fields not used for this 
-		algorithm</returns> 
-	'''
+    ''' <summary>
+        This is the entry point for the default solver
+        which just finds a valid random tour.  Note this could be used to find your
+        initial BSSF.
+        </summary>
+        <returns>results dictionary for GUI that contains three ints: cost of solution, 
+        time spent to find solution, number of permutations tried during search, the 
+        solution found, and three null values for fields not used for this 
+        algorithm</returns> 
+    '''
+    
+    def defaultRandomTour( self, time_allowance=60.0 ):
+        results = {}
+        cities = self._scenario.getCities()
+        ncities = len(cities)
+        foundTour = False
+        count = 0
+        bssf = None
+        start_time = time.time()
+        while not foundTour and time.time()-start_time < time_allowance:
+            # create a random permutation
+            perm = np.random.permutation( ncities )
+            route = []
+            # Now build the route using the random permutation
+            for i in range( ncities ):
+                route.append( cities[ perm[i] ] )
+            bssf = TSPSolution(route)
+            count += 1
+            if bssf.cost < np.inf:
+                # Found a valid route
+                foundTour = True
+        end_time = time.time()
+        results['cost'] = bssf.cost if foundTour else math.inf
+        results['time'] = end_time - start_time
+        results['count'] = count
+        results['soln'] = bssf
+        results['max'] = None
+        results['total'] = None
+        results['pruned'] = None
+        return results
+
+
+    ''' <summary>
+        This is the entry point for the greedy solver, which you must implement for 
+        the group project (but it is probably a good idea to just do it for the branch-and
+        bound project as a way to get your feet wet).  Note this could be used to find your
+        initial BSSF.
+        </summary>
+        <returns>results dictionary for GUI that contains three ints: cost of best solution, 
+        time spent to find best solution, total number of solutions found, the best
+        solution found, and three null values for fields not used for this 
+        algorithm</returns> 
+    '''
    #does the greedy algorithm using each node as a starting point. 
     #finds the shortest distance between each node not yet visited
     #returns the shortest path found
@@ -87,6 +87,7 @@ class TSPSolver:
         results = {}
         min_dist = math.inf
         min_path = []
+        valid_paths = []
         count = 0
         city_indice = 0
 
@@ -115,9 +116,10 @@ class TSPSolver:
 
                 if len(cities) == 0:
                     if curr_city.costTo(start_city) != math.inf:
-                        if dist < min_dist:
-                            min_dist = dist
-                            min_path = path.copy()
+                        valid_paths.append(path.copy())
+                       #if dist < min_dist:
+                       #    min_dist = dist
+                       #    min_path = path.copy()
 
                         count += 1
                     city_indice += 1
@@ -133,7 +135,6 @@ class TSPSolver:
                 curr_city = cities[min_index]
                 path.append(cities.pop(min_index))
 
-        solution = None
         cost = math.inf
         if len(min_path) > 0:
             solution = TSPSolution(min_path)
@@ -142,64 +143,61 @@ class TSPSolver:
             count = 0
 
         end_time = time.time()
-        results['cost'] = cost
-        results['time'] = end_time - start_time
-        results['count'] = count
-        results['soln'] = solution
+        print(valid_paths)
 
         
-        return results
-	
-	
-	
-	''' <summary>
-		This is the entry point for the branch-and-bound algorithm that you will implement
-		</summary>
-		<returns>results dictionary for GUI that contains three ints: cost of best solution, 
-		time spent to find best solution, total number solutions found during search (does
-		not include the initial BSSF), the best solution found, and three more ints: 
-		max queue size, total number of states created, and number of pruned states.</returns> 
-	'''
-		
-	def branchAndBound( self, time_allowance=60.0 ):
-		pass
+        return valid_paths
+    
+    
+    
+    ''' <summary>
+        This is the entry point for the branch-and-bound algorithm that you will implement
+        </summary>
+        <returns>results dictionary for GUI that contains three ints: cost of best solution, 
+        time spent to find best solution, total number solutions found during search (does
+        not include the initial BSSF), the best solution found, and three more ints: 
+        max queue size, total number of states created, and number of pruned states.</returns> 
+    '''
+        
+    def branchAndBound( self, time_allowance=60.0 ):
+        pass
 
 
 
-	''' <summary>
-		This is the entry point for the algorithm you'll write for your group project.
-		</summary>
-		<returns>results dictionary for GUI that contains three ints: cost of best solution, 
-		time spent to find best solution, total number of solutions found during search, the 
-		best solution found.  You may use the other three field however you like.
-		algorithm</returns> 
-	'''
-		
-	def getNewPath( self ):
-		#greedy, default... generate a bunch of paths
-		pass
+    ''' <summary>
+        This is the entry point for the algorithm you'll write for your group project.
+        </summary>
+        <returns>results dictionary for GUI that contains three ints: cost of best solution, 
+        time spent to find best solution, total number of solutions found during search, the 
+        best solution found.  You may use the other three field however you like.
+        algorithm</returns> 
+    '''
+        
+    def getNewPath( self ):
+        #greedy, default... generate a bunch of paths
+        pass
 
-	def checkFitness(self, path):
-		pass
+    def checkFitness(self, path):
+        pass
 
-	def selectWhichToCombine(self, allPaths):
-		#keep top 3, match 50% of others together
-		pass
+    def selectWhichToCombine(self, allPaths):
+        #keep top 3, match 50% of others together
+        pass
 
-	def crossOver(self, path1, path2):
-		#Mix two paths together
-		pass
+    def crossOver(self, path1, path2):
+        #Mix two paths together
+        pass
 
-	def mutate(self, path):
-		#do some random swaps on the path
-		pass
+    def mutate(self, path):
+        #do some random swaps on the path
+        pass
 
 
 
-		
-	def fancy( self,time_allowance=60.0 ):
-		pass
-		
+        
+    def fancy( self,time_allowance=60.0 ):
+        pass
+        
 
 
 
